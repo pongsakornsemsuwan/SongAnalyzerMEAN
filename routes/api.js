7f124a50883018cd4534f02fd37bfce4b2e82fc6
pongsakornsemsuwan <pongsakorn.semsuwan@gmail.com>
2014-12-24 16:45:25 +0700

    fix Songservice.js name case

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,4 @@
-var SongService = require('../service/SongService.js');
+var SongService = require('../service/songService.js');
 var ChordProgTree = require('../service/chordProgTreeService.js');
 var express = require('express');
 var router = express.Router();
